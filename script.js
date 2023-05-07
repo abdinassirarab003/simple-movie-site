41efd2b4370e37c8b0031b1cf5e3d27e6dc2a2bb
ABDINASSIR ARAB <abdinassirarab003@gmail.com>
2023-05-07 11:58:14 +0300

    1st M

--- script.js
+++ script.js
@@ -9,15 +9,53 @@ const search = document.getElementById ("query");
 returnMovies(APILINK)
 function returnMovies (url){
     fetch (url).then (res => res.json())
-    .then (function(data)){
+    .then (function(data){
         console.log(data.results);
+        
         data.results.forEach(element =>{
             const div_card = document.createElement ('div');
+            div_card.setAttribute ('class', 'card');
+
             const div_row = document.createElement ('div');
-            const div_colum = document.createElement ('div');
+            div_card.setAttribute ('class', 'row');
+
+            const div_column = document.createElement ('div');
+            div_card.setAttribute ('class', 'column');
+
             const image = document.createElement ('img');
+            div_card.setAttribute ('class', 'thumbnail');
+            div_card.setAttribute ('id', 'image');
+
             const title = document.createElement ('h3'); 
+            div_card.setAttribute ('id', 'title');
             const center = document.createElement('center');
+
+
+            title.innerHTML='$(element.title)';
+            img.src =IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_colum.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+            main.appendChild(div_row);
+
+
+
         });
     });
 }
+ form.addEventListener("submit", (e )=>{
+    e.preventDefault();
+    main.innerHTML ='';
+
+    const searchItem = search.value;
+    if (searchItem){
+
+     returnMovies(SEACRCHAPI + searchItem );
+     search.value=""
+
+    }
+ });
